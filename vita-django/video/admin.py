e3de583f3c4fd932f3fe11478b47c688c75dc2a2
harshjoeyit <harshit02gangwar@gmail.com>
2020-10-22 13:21:34 +0530

    Vote, Report models, views

--- vita-django/video/admin.py
+++ vita-django/video/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Video, Comment
+from .models import (Video, Comment, VideoCategory, 
+                    VideoVote, CommentVote, CommentReport, 
+                    VideoReport, ReportReason)
 # Register your models here.
 
 admin.site.register(Video)
 admin.site.register(Comment)
+admin.site.register([VideoCategory, VideoVote, CommentVote, 
+                    CommentReport, VideoReport, ReportReason])
--- vitareact/src/components/SingleComment.js
+++ vitareact/src/components/SingleComment.js
@@ -3,6 +3,19 @@ import React from 'react'
 // Returns a single comment
 // props -> single comment object
 
+// Like/Dislike
+    // 1. like/dislike only once
+    // 2. User cannot like or dislike his own comment
+    // 3. like/dislike toggle on consecutive clicks
+        // - if like click
+            // - if no like + 1
+            // - if liked already - no change
+            // - if dislike - remove dislike
+        // - if dislike click
+            // - if no dislik + 1
+            // - if liked already - remove like
+            // - if dislike - no change
+
 function SingleComment({comment}) {
     // TODO: 
     // make a component that returns username from id, 
