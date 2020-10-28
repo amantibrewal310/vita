4424e74f1cb03b917605fe975eb03aab824f007b
aman <amantibrewal310@gmail.com>
2020-10-28 14:42:59 +0530

    Braintree key hided

--- vita-django/payment/views.py
+++ vita-django/payment/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 import braintree
+from decouple import config
 # Create your views here.
 
 
 gateway = braintree.BraintreeGateway(
     braintree.Configuration(
         environment=braintree.Environment.Sandbox,
-        merchant_id='6nfmxd46bzdbtz2z',
-        public_key='yj5bvfvyyyq5rwr5',
-        private_key='c687933eda6474d19f7830f99c1b4ebe'
+        merchant_id=config('BRAINTREE_MERCHANT_ID'),
+        public_key=config('BRAINTREE_PUBLIC_KEY'),
+        private_key=config('BRAINTREE_PRIVATE_KEY')
     )
 )
