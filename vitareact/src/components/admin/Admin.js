d59d79f6da238865cf94be4cf52a2062bc27a12c
aman <amantibrewal310@gmail.com>
2020-11-01 04:01:18 +0530

    UI Changed

--- vitareact/src/components/admin/Admin.js
+++ vitareact/src/components/admin/Admin.js
@@ -1,141 +1,196 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
-import '../css/adminHome.css';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+// import '../css/adminHome.css';
 import axiosInstance from '../../axios';
 import VideoResults from './VideoResults';
 import ContentLoading from '../ContentLoading';
 import CommentResults from './CommentResults';
 import Header from '../Header';
+import { Col, Container, Row } from 'react-bootstrap';
+import Widget from '../Widget';
+import WidgetBar from '../WidgetBar';
+import WidgetPie from '../WidgetPie';
 
-// Admin Options Avalailable 
+// Admin Options Avalailable
 
 // Video options
-    // components
-        // 1. video result box [for search/filter results] / by default shows recently reported videos
-    // Features 
-        // 1. Upload new  video
-        // 2. Search for videos 
-        // 3. Filter, Sort for videos [by most reported, date/time ... etc]
-        // 4. Link to detail page
+// components
+// 1. video result box [for search/filter results] / by default shows recently reported videos
+// Features
+// 1. Upload new  video
+// 2. Search for videos
+// 3. Filter, Sort for videos [by most reported, date/time ... etc]
+// 4. Link to detail page
 
 // Comment options
-    // components 
-        // 1. comment result box [for filter results] / by default shows recently reported comments
-    // Featurs 
-        // 1. Filter, Sort for videos [by most reported, date/time... etc]
-        // 2. Link to detail page 
+// components
+// 1. comment result box [for filter results] / by default shows recently reported comments
+// Featurs
+// 1. Filter, Sort for videos [by most reported, date/time... etc]
+// 2. Link to detail page
 
 function Admin() {
+	const VideoResultsLoading = ContentLoading(VideoResults);
+	const CommentResultsLoading = ContentLoading(CommentResults);
 
-    const VideoResultsLoading = ContentLoading(VideoResults);
-    const CommentResultsLoading = ContentLoading(CommentResults);
+	const initHeading = {
+		videoHeading: 'Recent Video Reports',
+		commentHeading: 'Recent Comment Reports',
+	};
 
-    const initHeading = {
-        videoHeading: 'Recent Video Reports',
-        commentHeading: 'Recent Comment Reports'
-    }
+	const [boxHeading, setBoxHeading] = useState(initHeading);
+	const [videoResults, setVideoResults] = useState([]);
+	const [commentResults, setCommentResults] = useState([]);
+	const [videoResLoading, setVideoResLoadingLoading] = useState(false);
+	const [commentResLoading, setCommentResLoading] = useState(false);
 
-    const [boxHeading, setBoxHeading] = useState(initHeading);
-    const [videoResults, setVideoResults] = useState([]);
-    const [commentResults, setCommentResults] = useState([]);
-    const [videoResLoading, setVideoResLoadingLoading] = useState(false);
-    const [commentResLoading, setCommentResLoading] = useState(false);
+	useEffect(() => {
+		// recent reported videos
+		axiosInstance
+			.get(`video/reported-video-list/`)
+			.then((res) => {
+				setVideoResults(res.data);
+				// console.log(res.data)
+				setVideoResLoadingLoading(false);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-    useEffect(() => {
-        // recent reported videos 
-        axiosInstance
-            .get(`video/reported-video-list/`)
-            .then(res => {
-                setVideoResults(res.data);
-                // console.log(res.data)
-                setVideoResLoadingLoading(false);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }, [])
+	useEffect(() => {
+		axiosInstance
+			.get(`video/reported-comment-list/`)
+			.then((res) => {
+				setCommentResults(res.data);
+				// console.log(res.data)
+				setCommentResLoading(false);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-    useEffect(() => {
-        axiosInstance
-            .get(`video/reported-comment-list/`)
-            .then(res => {
-                setCommentResults(res.data);
-                // console.log(res.data)
-                setCommentResLoading(false);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }, [])
+	// search handlers
+	const handleSearchChange = () => {};
+	// search submit handler
+	const handleSearchSubmit = () => {};
+	const chartData = [
+		{
+			label: 'Venezuela',
+			value: '290',
+		},
+		{
+			label: 'Saudi',
+			value: '260',
+		},
+		{
+			label: 'Canada',
+			value: '180',
+		},
+		{
+			label: 'Iran',
+			value: '140',
+		},
+		{
+			label: 'Russia',
+			value: '115',
+		},
+	];
 
-    // search handlers
-    const handleSearchChange = () => {
-    }
-    // search submit handler
-    const handleSearchSubmit = () => {
-    }
+	return (
+		<>
+			<Header />
+			<div style={{ height: '60px' }}></div>
+			<h1 className='text-center'>Admin Panel</h1>
+			<Container>
+				<Row>
+					<Col>
+						<Widget
+							title={'Upload Video'}
+							description={'Create a new video'}
+							buttonName={'Upload'}
+							buttonPath={'/'}
+						/>
+					</Col>
+					<Col>
+						<Widget
+							title={'Total Video'}
+							description={'videos are uploaded'}
+							value={'100'}
+						/>
+					</Col>
+					<Col>
+						<Widget
+							title={'Membership'}
+							description={'Types of Membership available'}
+							value={'3'}
+						/>
+					</Col>
+				</Row>
+			</Container>
+			<Container>
+				<Row>
+					<WidgetBar data={chartData} title={'Subscription Graph'} />
+				</Row>
+			</Container>
+			<Container>
+				<Row>
+					<WidgetPie data={chartData} title={'Revenue Model Graph'} />
+				</Row>
+			</Container>
+			<div>
+				{/* <Link to={`/admin/create`}>
+					<button id='create-new-btn'>Create New Video</button>
+				</Link> */}
 
-    return (
-        <>
-        <Header />
-        <div>
-            <h1><u>Admin Home</u></h1>
-            
-            <Link 
-                to={`/admin/create`}>
-                <button 
-                    id="create-new-btn">
-                    Create New Video
-                </button>
-            </Link>
-            
-            <div id='search-container'>
-                <input 
-                    id='admin-search-box'
-                    type='text' 
-                    name='searchbox' 
-                    onChange={handleSearchChange}
-                    placeholder='Search Videos'
-                />
-                <button 
-                    id='admin-search-btn'
-                    type='submit'
-                    onClick={handleSearchSubmit}
-                >
-                    Search
-                </button>
-            </div>
+				{/* <div id='search-container'>
+					<input
+						id='admin-search-box'
+						type='text'
+						name='searchbox'
+						onChange={handleSearchChange}
+						placeholder='Search Videos'
+					/>
+					<button
+						id='admin-search-btn'
+						type='submit'
+						onClick={handleSearchSubmit}
+					>
+						Search
+					</button>
+				</div> */}
 
-            <div>  
-                <h3>Options</h3>
-                <button>Most views</button>
-                <button>Most Likes</button>
-                <button>Most Reported</button>
-            </div>
+				{/* <div>
+					<h3>Options</h3>
+					<button>Most views</button>
+					<button>Most Likes</button>
+					<button>Most Reported</button>
+				</div> */}
 
-            <div className="row">
-                <div className="col">
-                    {/* heading changes based on serach/filter applied */}
-                    <h2>{boxHeading.videoHeading}</h2>
-                    {/* video results */}
-                    <VideoResultsLoading 
-                        isLoading={videoResLoading}
-                        allVideos={videoResults}
-                    />
-                </div>
-                <div className="col">
-                    {/* heading changes based on serach/filter applied */}
-                    <h2>{boxHeading.commentHeading}</h2>
-                    {/* comments results */}
-                    <CommentResultsLoading 
-                        isLoading={commentResLoading}
-                        allComments={commentResults}
-                    />
-                </div>
-            </div>
-        </div>
-        </>
-    )
+				{/* <div className='row'> */}
+				{/* <div className='col'> */}
+				{/* heading changes based on serach/filter applied */}
+				{/* <h2>{boxHeading.videoHeading}</h2> */}
+				{/* video results */}
+				{/* <VideoResultsLoading
+							isLoading={videoResLoading}
+							allVideos={videoResults}
+						/> */}
+				{/* </div> */}
+				{/* <div className='col'> */}
+				{/* heading changes based on serach/filter applied */}
+				{/* <h2>{boxHeading.commentHeading}</h2> */}
+				{/* comments results */}
+				{/* <CommentResultsLoading
+							isLoading={commentResLoading}
+							allComments={commentResults}
+						/> */}
+				{/* </div> */}
+				{/* </div> */}
+			</div>
+		</>
+	);
 }
 
-export default Admin
+export default Admin;
--- vitareact/src/components/auth/Login/Login.js
+++ vitareact/src/components/auth/Login/Login.js
@@ -7,224 +7,228 @@ import FbLogin from 'react-facebook-login';
 import FacebookLogin from './FacebookLogin';
 // import checkLoggedIn from '../checkLoggedIn';
 import checkAdminLoggedIn from '../checkAdminLoggedIn';
-import { FB_CLIENT_ID, FB_CLIENT_SECRET, FB_APP_ID} from '../../../Backend';
+import { FB_CLIENT_ID, FB_CLIENT_SECRET, FB_APP_ID } from '../../../Backend';
 import formStyles from '../../css/forms.module.css';
 import Popup from '../../utils/Popup';
 import logo from '../../../images/vita-log.png';
 
-
 function Login() {
 	const initialFormData = Object.freeze({
 		email: '',
-    	password: '',
-    });
-    const intialErrors = {
-        credentialsError: null,
-        emptyFormError: null,
-    }
-    var expiry_time;
-    const history = useHistory();
+		password: '',
+	});
+	const intialErrors = {
+		credentialsError: null,
+		emptyFormError: null,
+	};
+	var expiry_time;
+	const history = useHistory();
 
 	const [formData, updateFormData] = useState(initialFormData);
-    const [error, setError] = useState(intialErrors);
-    const [showPopup, setShowPopup] = useState(false);    
+	const [error, setError] = useState(intialErrors);
+	const [showPopup, setShowPopup] = useState(false);
 
 	const handleChange = (e) => {
 		updateFormData({
 			...formData,
 			[e.target.name]: e.target.value,
 		});
-    };
-    
-    const handleSubmit = (e) => {
+	};
+
+	const handleSubmit = (e) => {
 		e.preventDefault();
-        
-        // validate form 
-        if(!vaidateDetails()) {
-            return;
-        }
-        // TODO:
-        // first check if the user exist 
-        // then process the response
-		AxiosLoginInstance
-			.post(`auth/token/`, {
-				grant_type: 'password',
-				username: formData.email.trim(),
-				password: formData.password.trim(),
-				client_id: FB_CLIENT_ID,
-			    client_secret: FB_CLIENT_SECRET,
-		    })
-			.then(res => {
-                // set the expiry of the token 
-                expiry_time = new Date().getTime() + res.data.expires_in * 1000;
-                localStorage.setItem('email', formData.email);
-                localStorage.setItem('expiry_time', expiry_time);
+
+		// validate form
+		if (!vaidateDetails()) {
+			return;
+		}
+		// TODO:
+		// first check if the user exist
+		// then process the response
+		AxiosLoginInstance.post(`auth/token/`, {
+			grant_type: 'password',
+			username: formData.email.trim(),
+			password: formData.password.trim(),
+			client_id: FB_CLIENT_ID,
+			client_secret: FB_CLIENT_SECRET,
+		})
+			.then((res) => {
+				// set the expiry of the token
+				expiry_time = new Date().getTime() + res.data.expires_in * 1000;
+				localStorage.setItem('email', formData.email);
+				localStorage.setItem('expiry_time', expiry_time);
 				localStorage.setItem('access_token', res.data.access_token);
-                localStorage.setItem('refresh_token', res.data.refresh_token);
-                setAdminStatus();
-                
-                if(checkAdminLoggedIn()) {
-                    setShowPopup(true);
-                    setTimeout(() => {
-                        history.push('/admin');
-                    }, 1500);
-               
-                } else {
-                    redirectBySubscription();
-                }
-            })
-            .catch(err => {
-                // wrong credentials 
-                setError({
-                    emptyFormError: null,
-                    credentialsError: err.data.error_description
-                });
-            });
-    };
+				localStorage.setItem('refresh_token', res.data.refresh_token);
+				setAdminStatus();
 
-    // TODO: 
-    // handle fb login error 
-    
-    const responseFacebook = async (response) => {
-        FacebookLogin(response.accessToken, response.email)
-        // TODO:
-        // check admin login
-        history.push('/');
+				if (checkAdminLoggedIn()) {
+					setShowPopup(true);
+					setTimeout(() => {
+						history.push('/admin');
+					}, 1500);
+				} else {
+					redirectBySubscription();
+				}
+			})
+			.catch((err) => {
+				// wrong credentials
+				setError({
+					emptyFormError: null,
+					credentialsError: err.data.error_description,
+				});
+			});
+	};
+
+	// TODO:
+	// handle fb login error
+
+	const responseFacebook = async (response) => {
+		FacebookLogin(response.accessToken, response.email);
+		// TODO:
+		// check admin login
+		history.push('/');
 		window.location.reload();
-    };
-    
-    // TODO: 
-    // add more validations 
-    // form validation 
-    const vaidateDetails = () => {
-        if(formData.email === "" || formData.password === "") {
-            // form is incomplete
-            setError({
-                credentialsError:null,
-                emptyFormError: 'All field must be filled!'
-            });
-            return false;
-        } else {
-            // form is filled 
-            setError({
-                ...error,
-                emptyFormError: null
-            });
-            return true;
-        }
-    }
+	};
 
-    const setAdminStatus = async () => {
-        const res = await axios.get(`http://127.0.0.1:8000/api/user/byemail/${formData.email}/`);
-        const admin = (res.data.is_staff ? '1' : '0');
-        localStorage.setItem('admin', admin);
-    } 
+	// TODO:
+	// add more validations
+	// form validation
+	const vaidateDetails = () => {
+		if (formData.email === '' || formData.password === '') {
+			// form is incomplete
+			setError({
+				credentialsError: null,
+				emptyFormError: 'All field must be filled!',
+			});
+			return false;
+		} else {
+			// form is filled
+			setError({
+				...error,
+				emptyFormError: null,
+			});
+			return true;
+		}
+	};
 
-    const redirectBySubscription = () => {
-        axiosInstance
-            .get(`http://127.0.0.1:8000/api/membership/user/type/`)
-            .then(res => {
-                if(res.data.membership_type === "None") {
-                    setShowPopup(true);
-                    setTimeout(() => {
-                        history.push("/subscribe");
-                    }, 1500);
-                } else {
-                    setTimeout(() => {
-                        history.push("/home");
-                    }, 1500);
-                }
-            })
-            .catch(err => {
-                console.log(err);
-                setError({
-                    ...error,
-                    credentialsError: 'could not login, please retry'
-                })
-                setTimeout(() => {
-                    window.location.reload()
-                }, 1000)    
-            })
-    }
+	const setAdminStatus = async () => {
+		const res = await axios.get(
+			`http://127.0.0.1:8000/api/user/byemail/${formData.email}/`
+		);
+		const admin = res.data.is_staff ? '1' : '0';
+		localStorage.setItem('admin', admin);
+	};
 
+	const redirectBySubscription = () => {
+		axiosInstance
+			.get(`http://127.0.0.1:8000/api/membership/user/type/`)
+			.then((res) => {
+				if (res.data.membership_type === 'None') {
+					setShowPopup(true);
+					setTimeout(() => {
+						history.push('/subscribe');
+					}, 1500);
+				} else {
+					setTimeout(() => {
+						history.push('/home');
+					}, 1500);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				setError({
+					...error,
+					credentialsError: 'could not login, please retry',
+				});
+				setTimeout(() => {
+					window.location.reload();
+				}, 1000);
+			});
+	};
 
-    return (
-        <div className={formStyles.formBG}>
-            <Popup show={showPopup} message="Welcome Back!" type="success"/>
-            <div className={formStyles.logoBox}>
-                <img src={logo} alt="logo" />
-            </div>
-            <div className={formStyles.formContainer}>
-                <h2 className={formStyles.heading}>Login</h2>
-                {
-                    (error.emptyFormError) 
-                    ? (<div className={formStyles.error}> {error.emptyFormError} </div>)
-                    : (<></>)
-                }
-                {
-                    (error.credentialsError)
-                    ? (<div className={formStyles.error}> {error.credentialsError} </div>)
-                    : (<></>)
-                }
-                <form className={formStyles.form}>
-                    <div>
-                        <input 
-                            type="text" 
-                            required
-                            name="email"
-                            placeholder='Email'
-                            onChange={handleChange}
-                            className={formStyles.input}
-                        />
-                    </div>
-                    <div>
-                        <input 
-                            type="password" 
-                            required
-                            name="password"
-                            placeholder='Password'
-                            onChange={handleChange}
-                            className={formStyles.input}
-                        />
-                    </div>
-                    <button 
-                        className={formStyles.submitBtn}
-                        type="submit"
-                        onClick={handleSubmit}
-                    >
-                        Log In    
-                    </button>
-                </form>
-                <div className={formStyles.fblogin}>
-                    <FbLogin
-		    	    	appId={FB_APP_ID}
-		    	    	fields="name,email,picture"
-		    	    	callback={responseFacebook}
-                        size="medium"
-		    	    />
-                </div>
-                <div className={formStyles.redirectLink}>
-                    <p>
-                        Not a member yet? 
-                    <Link to="../register">
-                        <span className={formStyles.redirectBtn}>
-                            <b> Sign Up </b>
-                        </span>
-                    </Link>
-                    </p>
-                </div>
-                <div className={formStyles.redirectLink}>
-                    <p>
-                    <Link to="../reset-password">
-                        <span className={formStyles.redirectBtn}>
-                            <b> Forgot Password </b>
-                        </span>
-                    </Link>
-                    </p>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className={formStyles.formBG}>
+			<Popup show={showPopup} message='Welcome Back!' type='success' />
+			<div className={formStyles.logoBox}>
+				<img src={logo} alt='logo' />
+			</div>
+			<div className={formStyles.formContainer}>
+				<h2 className={formStyles.heading}>Login</h2>
+				{error.emptyFormError ? (
+					<div className={formStyles.error}>
+						{' '}
+						{error.emptyFormError}{' '}
+					</div>
+				) : (
+					<></>
+				)}
+				{error.credentialsError ? (
+					<div className={formStyles.error}>
+						{' '}
+						{error.credentialsError}{' '}
+					</div>
+				) : (
+					<></>
+				)}
+				<form className={formStyles.form}>
+					<div>
+						<input
+							type='text'
+							required
+							name='email'
+							placeholder='Email'
+							onChange={handleChange}
+							className={formStyles.input}
+						/>
+					</div>
+					<div>
+						<input
+							type='password'
+							required
+							name='password'
+							placeholder='Password'
+							onChange={handleChange}
+							className={formStyles.input}
+						/>
+					</div>
+					<button
+						className={formStyles.submitBtn}
+						type='submit'
+						onClick={handleSubmit}
+					>
+						Log In
+					</button>
+				</form>
+				<div className={formStyles.fblogin}>
+					<FbLogin
+						appId={FB_APP_ID}
+						fields='name,email,picture'
+						callback={responseFacebook}
+						size='medium'
+					/>
+				</div>
+				<div className={formStyles.redirectLink}>
+					<p>
+						Not a member yet?
+						<Link to='../register'>
+							<span className={formStyles.redirectBtn}>
+								<b> Sign Up </b>
+							</span>
+						</Link>
+					</p>
+				</div>
+				<div className={formStyles.redirectLink}>
+					<p>
+						<Link to='../reset-password'>
+							<span className={formStyles.redirectBtn}>
+								<b> Forgot Password </b>
+							</span>
+						</Link>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Login
+export default Login;
