eaacf1386d20f58dfaf55cc528a8e3c462fee1c6
aman <amantibrewal310@gmail.com>
2020-11-01 13:57:19 +0530

    Video Detail Page fixed

--- vitareact/src/components/admin/CreateVideo.js
+++ vitareact/src/components/admin/CreateVideo.js
@@ -1,310 +1,331 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axiosInstance from '../../axios';
 import axios from 'axios';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import formStyles from '../css/forms.module.css';
 import Popup from '../utils/Popup';
+import Header from '../Header';
 
 function CreateVideo() {
+	// TODO:
+	// 1.
+	// add form validation for file types
+	// both in react and django, add error types for files
+	// 2.
+	// check for only admin upload
+	const history = useHistory();
 
-    // TODO:
-    // 1.
-    // add form validation for file types 
-    // both in react and django, add error types for files 
-    // 2.
-    // check for only admin upload
-    const history = useHistory();
+	const initTextData = Object.freeze({
+		title: '',
+		description: '',
+		playtime: 0,
+		category: -1,
+	});
+	const initErrors = {
+		emptyFormError: null,
+		authError: null,
+	};
+	const initUploadState = {
+		uploadProgress: 0,
+		source: null,
+	};
 
-    const initTextData = Object.freeze({
-        title: '',
-        description: '',
-        playtime: 0,
-        category: -1
-    });
-    const initErrors = {
-        emptyFormError: null,
-        authError: null
-    }
-    const initUploadState = {
-        uploadProgress: 0,
-        source:null
-    }
+	// states
+	// category options
+	const [categoryOptions, setCategoryOptions] = useState([]);
+	// form data
+	const [textData, setTextData] = useState(initTextData);
+	const [thumbnail, setThumbnail] = useState(null);
+	const [videoFile, setVideoFile] = useState(null);
+	// form error
+	const [error, setError] = useState(initErrors);
+	// form upload
+	const [uploadState, setUploadState] = useState(initUploadState);
+	// popup
+	const [showPopup, setShowPopup] = useState(false);
 
-    // states
-    // category options
-    const [categoryOptions, setCategoryOptions] = useState([])
-    // form data 
-    const [textData, setTextData] = useState(initTextData)
-    const [thumbnail, setThumbnail] = useState(null);
-    const [videoFile, setVideoFile] = useState(null);
-    // form error
-    const [error, setError] = useState(initErrors);
-    // form upload
-    const [uploadState, setUploadState] = useState(initUploadState);
-    // popup
-    const [showPopup, setShowPopup] = useState(false); 
+	useEffect(() => {
+		// getting category options
+		axiosInstance
+			.get(`video/categories/`)
+			.then((res) => {
+				setCategoryOptions(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-    useEffect(() => {
-        // getting category options
-        axiosInstance
-            .get(`video/categories/`)
-            .then(res => {
-                setCategoryOptions(res.data);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }, []);
+	// listener
+	const handleChange = (e) => {
+		if ([e.target.name] == 'thumbnail') {
+			setThumbnail({
+				thumbnail: e.target.files,
+			});
+		} else if ([e.target.name] == 'videoFile') {
+			setVideoFile({
+				videoFile: e.target.files,
+			});
+		} else {
+			setTextData({
+				...textData,
+				[e.target.name]: e.target.value,
+			});
+		}
+	};
 
-    // listener
-    const handleChange = (e) => {
-        if([e.target.name] == 'thumbnail') {
-            setThumbnail({
-                thumbnail: e.target.files
-            });
-        } else if([e.target.name] == 'videoFile') {
-            setVideoFile({
-                videoFile: e.target.files
-            });
-        } else {
-            setTextData({
-                ...textData,
-                [e.target.name]: e.target.value
-            });
-        }
-    }
+	// listener
+	// TODO:
+	// upload progress bar
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-    // listener
-    // TODO: 
-    // upload progress bar 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+		// validate form
+		if (!vaidateDetails()) {
+			return;
+		}
 
-        // validate form
-        if(!vaidateDetails()) {
-            return;
-        }
+		let formData = new FormData();
 
-        let formData = new FormData();
+		formData.append('title', textData.title.trim());
+		formData.append('description', textData.description.trim());
+		formData.append('category', textData.category);
+		formData.append('playtime', textData.playtime);
+		formData.append('thumbnail', thumbnail.thumbnail[0]);
+		formData.append('videoFile', videoFile.videoFile[0]);
 
-        formData.append('title', textData.title.trim());
-        formData.append('description', textData.description.trim());
-        formData.append('category', textData.category);
-        formData.append('playtime', textData.playtime);
-        formData.append('thumbnail',thumbnail.thumbnail[0]);
-        formData.append('videoFile', videoFile.videoFile[0]);
+		// upload progress
+		const cancelToken = axios.CancelToken;
+		const source = cancelToken.source();
+		setUploadState({
+			...uploadState,
+			source: source,
+		});
 
-        // upload progress
-        const cancelToken = axios.CancelToken;
-        const source = cancelToken.source();
-        setUploadState({
-            ...uploadState,
-            source: source
-        });
+		// TODO:
+		// progress not in sync with upload
+		// try putting a loader in place of progress bar
 
-        // TODO: 
-        // progress not in sync with upload 
-        // try putting a loader in place of progress bar 
+		// config for upload progress
+		let config = {
+			onUploadProgress: function (progressEvent) {
+				let percentCompleted = Math.round(
+					(progressEvent.loaded * 100) / progressEvent.total
+				);
+				setUploadState({
+					...uploadState,
+					uploadProgress: percentCompleted,
+				});
+			},
+			cancelToken: source.token,
+		};
 
-        // config for upload progress
-        let config = {
-            onUploadProgress: function(progressEvent) {
-                let percentCompleted = Math.round( (progressEvent.loaded * 100) / progressEvent.total);
-                setUploadState({
-                    ...uploadState,
-                    uploadProgress: percentCompleted
-                });
-            },
-            cancelToken: source.token
-        };
+		// send post request to create video
+		axiosInstance
+			.post(`video/video-list/`, formData, config)
+			.then((res) => {
+				setShowPopup(true);
+				setTimeout(() => {
+					history.push({
+						pathname: `/admin/video-details/${res.data.id}`,
+					});
+					window.location.reload();
+				}, 1500);
+			})
+			.catch((err) => {
+				console.log(err);
+				if (
+					err.data.detail ==
+					'Invalid token header. No credentials provided.'
+				) {
+					setError({
+						emptyFormError: null,
+						authError: 'No credentials provided, unauthorized.',
+					});
+				}
+			});
+	};
 
-        // send post request to create video 
-        axiosInstance
-            .post(`video/video-list/`, formData, config)
-            .then(res => {
-                setShowPopup(true);
-                setTimeout(() => {
-                    history.push({
-                        pathname: `/admin/video-details/${res.data.id}`,
-                    });
-                    window.location.reload();
-                }, 1500);
-            })
-            .catch(err => {
-                console.log(err);
-                if(err.data.detail == "Invalid token header. No credentials provided.") {
-                    setError({
-                        emptyFormError: null,
-                        authError: 'No credentials provided, unauthorized.'
-                    });
-                }
-            });
-    }
+	// upload cancel handler
+	const handleCancelUpload = () => {
+		uploadState.source.cancel();
+		setUploadState({
+			source: null,
+			uploadProgress: 0,
+		});
+	};
 
-    // upload cancel handler 
-    const handleCancelUpload = () => {
-        uploadState.source.cancel();
-        setUploadState({
-            source: null,
-            uploadProgress: 0
-        });
-    }
+	// form validators
+	const vaidateDetails = () => {
+		if (
+			textData.title === '' ||
+			textData.description === '' ||
+			textData.category === -1 ||
+			textData.playtime <= 0 ||
+			thumbnail === null ||
+			videoFile === null
+		) {
+			// update the errors in form
+			setError({
+				authError: null,
+				emptyFormError: 'Fill all fileds, upload all files',
+			});
+			return false;
+		} else {
+			// form is completely filled
+			setError({
+				authError: null,
+				emptyFormError: null,
+			});
+			return true;
+		}
+	};
 
-    // form validators 
-    const vaidateDetails = () => {
-        if (textData.title === "" || textData.description === "" || 
-            textData.category === -1 || textData.playtime <= 0 ||
-            thumbnail === null || videoFile === null) {
-            // update the errors in form
-            setError({
-                authError: null,
-                emptyFormError: 'Fill all fileds, upload all files'
-            });
-            return false;
-        } else {
-            // form is completely filled
-            setError({
-                authError:null,
-                emptyFormError: null
-            });
-            return true;
-        }
-    }
-
-    return (
-        <div className={formStyles.formBG}>
-            <Popup show={showPopup} message="Video Uploaded Successfully" type="success"/>
-            <div className={formStyles.videoFormContainer}>
-                <h2 className={formStyles.heading}>Create new video</h2>
-                {/* TODO:
+	return (
+		<>
+			<Header />
+			<div style={{ height: '60px' }}></div>
+			<div className={formStyles.formBG}>
+				<Popup
+					show={showPopup}
+					message='Video Uploaded Successfully'
+					type='success'
+				/>
+				<div className={formStyles.videoFormContainer}>
+					<h2 className={formStyles.heading}>Create new video</h2>
+					{/* TODO:
                     create seperate error component, 
                     to be used it in all forms
                 */}
-                {
-                    (error.authError) 
-                    ? (<div className={formStyles.error}> {error.authError} </div>)
-                    : (<></>)
-                }
-                {
-                    (error.emptyFormError)
-                    ? (<div className={formStyles.error}> {error.emptyFormError} </div>)
-                    : (<></>)
-                }
-                {
-                    (uploadState.uploadProgress > 0) 
-                    ? (
-                        <div className={formStyles.uploadProgress}>
-                            <div  className={formStyles.error}>Upload: {uploadState.uploadProgress}%</div>
-                            <button 
-                                className={formStyles.videoSubmitBtn}
-                                type="submit"
-                                onClick={handleCancelUpload}
-                            >
-                                Cancel
-                            </button>
-                        </div>
-                    ) : (
-                        <></>
-                    )
-                }
-                
-                <div className={formStyles.videoFormRow}>
-                    <div className={formStyles.videoFormCol}>
-                        <div>
-                            <input 
-                                type="text" 
-                                name="title"
-                                required
-                                placeholder="Video title"
-                                onChange={handleChange}
-                                className={formStyles.videoInput}
-                            />
-                        </div>
-                        <div>
-                            <textarea 
-                                rows="10"
-                                name="description"
-                                required
-                                placeholder="Storyline"
-                                onChange={handleChange}
-                                className={formStyles.videoInput}
-                                style={{resize: 'none'}}
-                            /> 
-                        </div>
-                    </div>
-                    <div className={formStyles.videoFormCol}>
-                        <div>
-                            <input 
-                                className={formStyles.videoInput}
-                                type="file"
-                                accept="image/*" 
-                                id="thumbnail"
-                                name="thumbnail"
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <input 
-                                className={formStyles.videoInput}
-                                type="file"
-                                accept="video/*" 
-                                id="videoFile"
-                                name="videoFile"
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <input 
-                                className={formStyles.videoInput}
-                                type="number" 
-                                name="playtime"
-                                required
-                                placeholder="Play time (minutes)"
-                                onChange={handleChange}
-                                min='0'
-                            /> 
-                        </div>
-                        <div className={formStyles.videoSelectInputContainer}>
-                            <select 
-                                value={textData.category} 
-                                onChange={(e) => {
-                                    setTextData({
-                                        ...textData,
-                                        category: e.target.value
-                                    })
-                                }}>
-                                <option 
-                                    key='0'
-                                    value='-1'
-                                > 
-                                    Select
-                                </option>
-                                {
-                                    categoryOptions.map(option => (
-                                        <option 
-                                            key={option.id} 
-                                            value={option.id}
-                                        > 
-                                            {option.category}
-                                        </option>
-                                    ))
-                                }
-                            </select>
-                        </div>
-                    </div>
-                    <button 
-                        type="submit"
-                        onClick={handleSubmit}
-                        className={formStyles.videoSubmitBtn}
-                    >
-                        Create    
-                    </button> 
-                </div>
-            </div>
-        </div>
-    )
+					{error.authError ? (
+						<div className={formStyles.error}>
+							{' '}
+							{error.authError}{' '}
+						</div>
+					) : (
+						<></>
+					)}
+					{error.emptyFormError ? (
+						<div className={formStyles.error}>
+							{' '}
+							{error.emptyFormError}{' '}
+						</div>
+					) : (
+						<></>
+					)}
+					{uploadState.uploadProgress > 0 ? (
+						<div className={formStyles.uploadProgress}>
+							<div className={formStyles.error}>
+								Upload: {uploadState.uploadProgress}%
+							</div>
+							<button
+								className={formStyles.videoSubmitBtn}
+								type='submit'
+								onClick={handleCancelUpload}
+							>
+								Cancel
+							</button>
+						</div>
+					) : (
+						<></>
+					)}
+
+					<div className={formStyles.videoFormRow}>
+						<div className={formStyles.videoFormCol}>
+							<div>
+								<input
+									type='text'
+									name='title'
+									required
+									placeholder='Video title'
+									onChange={handleChange}
+									className={formStyles.videoInput}
+								/>
+							</div>
+							<div>
+								<textarea
+									rows='10'
+									name='description'
+									required
+									placeholder='Storyline'
+									onChange={handleChange}
+									className={formStyles.videoInput}
+									style={{ resize: 'none' }}
+								/>
+							</div>
+						</div>
+						<div className={formStyles.videoFormCol}>
+							<div>
+								<input
+									className={formStyles.videoInput}
+									type='file'
+									accept='image/*'
+									id='thumbnail'
+									name='thumbnail'
+									onChange={handleChange}
+									required
+								/>
+							</div>
+							<div>
+								<input
+									className={formStyles.videoInput}
+									type='file'
+									accept='video/*'
+									id='videoFile'
+									name='videoFile'
+									onChange={handleChange}
+									required
+								/>
+							</div>
+							<div>
+								<input
+									className={formStyles.videoInput}
+									type='number'
+									name='playtime'
+									required
+									placeholder='Play time (minutes)'
+									onChange={handleChange}
+									min='0'
+								/>
+							</div>
+							<div
+								className={formStyles.videoSelectInputContainer}
+							>
+								<select
+									value={textData.category}
+									onChange={(e) => {
+										setTextData({
+											...textData,
+											category: e.target.value,
+										});
+									}}
+								>
+									<option key='0' value='-1'>
+										Select
+									</option>
+									{categoryOptions.map((option) => (
+										<option
+											key={option.id}
+											value={option.id}
+										>
+											{option.category}
+										</option>
+									))}
+								</select>
+							</div>
+						</div>
+						<button
+							type='submit'
+							onClick={handleSubmit}
+							className={formStyles.videoSubmitBtn}
+						>
+							Create
+						</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default CreateVideo
+export default CreateVideo;
--- vitareact/src/components/admin/VideoDetails.js
+++ vitareact/src/components/admin/VideoDetails.js
@@ -1,60 +1,60 @@
 import React from 'react';
-import {Link, useParams} from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom';
 import GetVideo from '../GetVideo';
 import Header from '../Header';
 import DeleteVidCom from './DeleteVidCom';
 import EditVideo from './EditVideo';
 import ReportList from './ReportListVidCom';
-import formStyles from '../css/forms.module.css'
+import formStyles from '../css/forms.module.css';
 
 // Video [id]
-    // 1. Video player
-    // 2. Video details [including comments] >> Reuse the GetVideo Components
+// 1. Video player
+// 2. Video details [including comments] >> Reuse the GetVideo Components
 
 const detailBox = {
-    paddingBottom: '40px'
-}
+	paddingBottom: '40px',
+};
 
 const heading = {
-    padding: '10px',
-    textAlign: 'center'
-}
-
+	padding: '10px',
+	textAlign: 'center',
+};
 
 function VideoDetails() {
-    // get id from pramas 
-    const {id} = useParams();
+	// get id from pramas
+	const { id } = useParams();
 
-    return (
-        <>
-        <Header />
-        <div style={detailBox}>
-            
-            <h1 style={heading}>Video Details</h1>
-            <br />
-            
-            <EditVideo id={id} />
-            
-            <GetVideo />
-            
-            <h2 style={heading}>Report List</h2>
-            <br />
-            
-            <ReportList type="video" id={id} />
-            
-            <Link to={`../video-report-action/${id}`}>
-                <button 
-                    className={formStyles.smallSubmitBtn}
-                >
-                    View Reports 
-                </button>
-            </Link>
+	return (
+		<>
+			<Header />
+			<div style={{ height: '60px' }}></div>
+			<div style={detailBox}>
+				<h1 style={heading}>Video Details</h1>
+				<br />
 
-            <DeleteVidCom type="Video" urlSuffix="video/video-list/" id={id} />
-        
-        </div>
-        </>
-    )
+				<EditVideo id={id} />
+
+				<GetVideo />
+
+				<h2 style={heading}>Report List</h2>
+				<br />
+
+				<ReportList type='video' id={id} />
+
+				<Link to={`../video-report-action/${id}`}>
+					<button className={formStyles.smallSubmitBtn}>
+						View Reports
+					</button>
+				</Link>
+
+				<DeleteVidCom
+					type='Video'
+					urlSuffix='video/video-list/'
+					id={id}
+				/>
+			</div>
+		</>
+	);
 }
 
-export default VideoDetails
+export default VideoDetails;
