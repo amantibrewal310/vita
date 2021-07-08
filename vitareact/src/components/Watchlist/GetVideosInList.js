f5f49c18e37040a8b03054b53541336119b0e7f6
Aman <amantibrewal@gmail.com>
2021-07-09 00:41:48 +0530

    backend code separate

--- vitareact/src/components/Watchlist/GetVideosInList.js
+++
@@ -1,7 +0,0 @@
-import axiosInstance from '../../axios';
-
-async function GetVideosInList(id) {
-	return await axiosInstance.get(`video/watchlist/${id}/`);
-}
-
-export default GetVideosInList;
--- vitareact/src/components/auth/checkAdminLoggedIn.js
+++
@@ -1,6 +0,0 @@
-
-function checkAdminLoggedIn() {
-    return (localStorage.getItem('admin') == '1')
-}
-
-export default checkAdminLoggedIn
