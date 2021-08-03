0961e0cf5878bff3023bae5ee50b41c17a23d7cb
Aman <amantibrewal@gmail.com>
2021-08-03 11:31:21 +0530

    code refactor

--- src/components/admin/DeleteVidCom.js
+++ src/components/admin/DeleteVidCom.js
@@ -1,45 +1,47 @@
-import React from 'react'
-import {useHistory} from 'react-router-dom'
-import axiosInstance from '../../axios';
-import formStyles from '../css/forms.module.css'
+import React from "react";
+import { useHistory } from "react-router-dom";
+import axiosInstance from "../../axios";
+import formStyles from "../css/forms.module.css";
 
-// Deletes video/comment 
+// Deletes video/comment
 // props - type, id, urlSuffix
 
-function DeleteVidCom({type, id, urlSuffix}) {
-    const history = useHistory();
-    
-    const handleDelete = () => {
-        // take confirmation
-        const confirmDelete = window.confirm(`This ${type} will be permanently removed!`);
-        if(confirmDelete) {
-            axiosInstance
-            .delete(`${urlSuffix}${id}/`)
-            .then(res => {
-                if(res.status == 204) {
-                    // TODO:
-                    // show success message ${type} deleted
-                    history.push('/admin');
-                    window.location.reload();
-                } else {
-                    console.log(`${type} not deleted`);
-                }
-            })
-            .catch(err => {
-                console.log(`could not delete ${type}, not found`);
-            });
-        } 
-    }
+function DeleteVidCom({ type, id, urlSuffix }) {
+  const history = useHistory();
 
-    return (
-        <button 
-            onClick={handleDelete}
-            className={formStyles.dangerBtn}
-            style={{display: 'block', marginTop: '20px'}}
-        >
-            Delete {type}
-        </button>
-    )
+  const handleDelete = () => {
+    // take confirmation
+    const confirmDelete = window.confirm(
+      `This ${type} will be permanently removed!`
+    );
+    if (confirmDelete) {
+      axiosInstance
+        .delete(`${urlSuffix}${id}/`)
+        .then((res) => {
+          if (res.status === 204) {
+            // TODO:
+            // show success message ${type} deleted
+            history.push("/admin");
+            window.location.reload();
+          } else {
+            console.log(`${type} not deleted`);
+          }
+        })
+        .catch((err) => {
+          console.log(`could not delete ${type}, not found`);
+        });
+    }
+  };
+
+  return (
+    <button
+      onClick={handleDelete}
+      className={formStyles.dangerBtn}
+      style={{ display: "block", marginTop: "20px" }}
+    >
+      Delete {type}
+    </button>
+  );
 }
 
-export default DeleteVidCom
+export default DeleteVidCom;
--- src/components/paymentHelper.js
+++ src/components/paymentHelper.js
@@ -1,5 +1,5 @@
 // import { API } from '../../backend';
-import axiosInstance from '../axios';
+import axiosInstance from "../axios";
 // export const getMeToken = () => {
 // 	return fetch(`${API}payment/gettoken/`, {
 // 		method: 'GET',
@@ -11,20 +11,20 @@ import axiosInstance from '../axios';
 // };
 
 export const processPayment = (paymentInfo) => {
-	const formData = new FormData();
+  const formData = new FormData();
 
-	for (const name in paymentInfo) {
-		formData.append(name, paymentInfo[name]);
-	}
+  for (const name in paymentInfo) {
+    formData.append(name, paymentInfo[name]);
+  }
 
-	return axiosInstance
-		.post(`payment/process/`, formData)
-		.then((res) => {
-			console.log(res);
-			console.log(res.data);
-			if (res.data.success) {
-				return res.data;
-			}
-		})
-		.catch((error) => console.log(error));
+  return axiosInstance
+    .post(`payment/process/`, formData)
+    .then((res) => {
+      console.log(res);
+      console.log(res.data);
+      if (res.data.success) {
+        return res.data;
+      }
+    })
+    .catch((error) => console.log(error));
 };
